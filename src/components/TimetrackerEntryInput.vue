68328a36ec90110a318de4b3bae5531a4a401db0
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2017-08-20 15:35:18 +0200

    Add some styling

--- src/components/TimetrackerEntryInput.vue
+++ src/components/TimetrackerEntryInput.vue
@@ -1,13 +1,45 @@
 <template>
-  <div id="timetracking-input">
-    <b-form inline.boolean="true">
-      <b-form-input placeholder="Datum" @keyup.enter="save(item)" v-model="item.date"></b-form-input>
-      <b-form-input placeholder="Startzeit" @keyup.enter="save(item)" v-model="item.start_time"></b-form-input>
-      <b-form-input placeholder="Endzeit" @keyup.enter="save(item)" v-model="item.end_time"></b-form-input>
-      <b-form-input placeholder="Beschreibung" @keyup.enter="save(item)" v-model="item.description"></b-form-input>
+  <div class="container-fluid align-items-center timetracking-input">
+
+  <b-card title="Zeit erfassen" class="justify-content-center">
+      <p class="card-text muted">
+        In der untenstehenden Form können Zeiten erfasst oder bearbeitet werden.
+      </p>
+      <div class="align-items-center align-self-center">
+        <b-form class="row">
+          <div class="col-1">
+            <b-form-group label="Datum" label-text-align="left">
+              <b-form-input placeholder="Datum" @keyup.enter="save(item)" v-model="item.date"></b-form-input>
+            </b-form-group>
+          </div>
+
+          <div class="col-1">
+            <b-form-group label="Startzeit" label-text-align="left">
+              <b-form-input placeholder="Startzeit" @keyup.enter="save(item)" v-model="item.start_time"></b-form-input>
+            </b-form-group>
+          </div>
+
+          <div class="col-1">
+            <b-form-group label="Endzeit" label-text-align="left">
+              <b-form-input placeholder="Endzeit" @keyup.enter="save(item)" v-model="item.end_time"></b-form-input>
+            </b-form-group>
+          </div>
+
+          <div class="col-8">
+            <b-form-group label="Beschreibung" label-text-align="left">
+              <b-form-input placeholder="Beschreibung" @keyup.enter="save(item)" v-model="item.description"></b-form-input>
+            </b-form-group>
+          </div>
+        </b-form>
+      </div>
+
+    <template slot="footer">
       <b-button variant="primary" @click="save(item)">{{ this.item.id != undefined  ? "Ändern" : "Neu anlegen" }}</b-button>
-    </b-form>
+    </template>
+
+  </b-card>
   </div>
+
 </template>
 
 <script>
